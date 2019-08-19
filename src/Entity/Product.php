1c1411612fb5cbe638430e151d52d54eb21c616c
Daniel Formela <daniel8702@gmail.com>
2019-08-19 17:02:00 +0200

    Remove annotations and add orm config Products

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,22 +2,14 @@
 
 declare(strict_types=1);
 
-namespace App\Entity\Product;
+namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product")
- */
 class Product extends BaseProduct
 {
-    /**
-     * @ORM\Column(type="string", nullable=false)
-     * @var string
-     */
+    /** @var string */
     private $color;
 
     /** @return string|null */
--- src/Entity/ProductTranslation.php
+++ src/Entity/ProductTranslation.php
@@ -2,15 +2,10 @@
 
 declare(strict_types=1);
 
-namespace App\Entity\Product;
+namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ProductTranslation as BaseProductTranslation;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product_translation")
- */
 class ProductTranslation extends BaseProductTranslation
 {
 }
